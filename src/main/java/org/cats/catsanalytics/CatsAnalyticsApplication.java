803b058cbb8149d20739e507d34dfad654a7e015
Yared Ayalew <yareda@gmail.com>
2017-09-28 14:34:04 +0300

    Create project structure

---
+++ src/main/java/org/cats/catsanalytics/CatsAnalyticsApplication.java
@@ -0,0 +1,12 @@
+package org.cats.catsanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatsAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatsAnalyticsApplication.class, args);
+	}
+}
---
+++ src/test/java/org/cats/catsanalytics/CatsAnalyticsApplicationTests.java
@@ -0,0 +1,16 @@
+package org.cats.catsanalytics;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CatsAnalyticsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
